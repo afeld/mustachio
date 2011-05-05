ec21cab3d8f5de413df49bbd5a7dd079042a8ffe
Aidan Feldman <aidan.feldman@gmail.com>
2011-05-05 03:30:07 -0400

    greatly simplified stache placement & rotation w/ affine transform

--- mustachio.rb
+++ mustachio.rb
@@ -5,7 +5,7 @@ require 'ruby-debug'
 
 # thanks to http://therantsandraves.com/?p=602 for the 'staches
 MUSTACHE_FILENAME = File.expand_path(File.join('public', 'images', 'mustache_03.png'))
-MUSTACHE_WIDTH = ImageSize.new(File.new(MUSTACHE_FILENAME)).get_width
+MUSTACHE_WIDTH, MUSTACHE_HEIGHT = ImageSize.new(File.new(MUSTACHE_FILENAME)).get_size
 
 FACE_POS_ATTRS = ['center', 'eye_left', 'eye_right', 'mouth_left', 'mouth_center', 'mouth_right', 'nose']
 
@@ -33,22 +33,21 @@ Magickly.dragonfly.configure do |c|
     
     commands = []
     photo_data['tags'].each do |face|
-      mouth_width = face['mouth_right']['x'] - face['mouth_left']['x']
-      lip_height = face['mouth_center']['y'] - face['nose']['y']
+      # perform affine transform, such that the top-center
+      # of the mustache is mapped to the nose, and the bottom-center
+      # of the stache is mapped to the center of the mouth
+      affine_params = [
+        [MUSTACHE_WIDTH/2, 0],
+        [face['nose']['x'], face['nose']['y']],
+        
+        [MUSTACHE_WIDTH/2, MUSTACHE_HEIGHT],
+        [face['mouth_center']['x'], face['mouth_center']['y']]
+      ]
+      affine_params_str = affine_params.map{|p| p.join(',') }.join(' ')
       
-      stache_width = mouth_width * (rand * 2 + 1.2)
-      stache_scale = stache_width / MUSTACHE_WIDTH
-      
-      angle = Math.atan(
-        (face['mouth_center']['x'] - face['nose']['x']) /
-        (face['mouth_center']['y'] - face['nose']['y'])
-      ) / Math::PI * -360
-      
-      x = face['nose']['x'] - (stache_width / 2)
-      y = face['nose']['y'] #+ (lip_height * 0.2)
-      
-      commands << "\\( \\( #{MUSTACHE_FILENAME} -geometry +#{x.to_i}+#{y.to_i} \\) +distort SRT '0,0 #{stache_scale} #{angle}' \\) -composite"
+      commands << "\\( #{MUSTACHE_FILENAME} +distort Affine '#{affine_params_str}' \\)"
     end
+    commands << "-layers merge"
     
     command_str = commands.join(' ')
     process :convert, command_str
