7976060d0fb8d88f863f5cd17f8ca79aaf792890
Aidan Feldman <aidan.feldman@gmail.com>
2011-05-05 06:00:06 -0400

    encode uri in bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -7,12 +7,12 @@ javascript:(function(){
     var image = images[i];
     var src = image.getAttribute('src');
     if (image.src[0] === '/'){
-      image.setAttribute('src', URL + window.location.origin + src);
+      image.setAttribute('src', URL + encodeURIComponent(window.location.origin + src));
     } else if (src.match(/^https?:\/\//)) {
-      image.setAttribute('src', URL + src);
+      image.setAttribute('src', URL + encodeURIComponent(src));
     }
   }
 })();
 
 // JSMin'd
-javascript:(function(){var URL='http://mustachio.heroku.com/magickly?mustachify=true&src=';var images=document.getElementsByTagName('img');var i;for(i=0;i<images.length;i+=1){var image=images[i];var src=image.getAttribute('src');if(image.src[0]==='/'){image.setAttribute('src',URL+window.location.origin+src);}else if(src.match(/^https?:\/\//)){image.setAttribute('src',URL+src);}}})();
+javascript:(function(){var URL='http://mustachio.heroku.com/magickly?mustachify=true&src=';var images=document.getElementsByTagName('img');var i;for(i=0;i<images.length;i+=1){var image=images[i];var src=image.getAttribute('src');if(image.src[0]==='/'){image.setAttribute('src',URL+encodeURIComponent(window.location.origin+src));}else if(src.match(/^https?:\/\//)){image.setAttribute('src',URL+encodeURIComponent(src));}}})();
