d92b3a3dbc9310b126b04d1684f2e7ffe2c0a3db
Aidan Feldman <aidan.feldman@gmail.com>
2011-05-05 02:27:36 -0400

    big spec refactoring

--- spec/unit/analyser_spec.rb
+++ spec/unit/analyser_spec.rb
@@ -10,23 +10,39 @@ describe "retrieving face data" do
     Magickly.dragonfly.fetch(image_url)
   end
   
+  def face_data(filename='dubya.jpeg')
+    photo = get_photo(filename)
+    data = nil
+    VCR.use_cassette( filename.chomp(File.extname(filename)) ) do
+      data = photo.face_data
+    end
+    
+    data
+  end
+  
+  def face_data_as_px(filename='dubya.jpeg')
+    photo = get_photo(filename)
+    data = nil
+    VCR.use_cassette( filename.chomp(File.extname(filename)) ) do
+      data = photo.face_data_as_px
+    end
+    
+    data
+  end
+  
   describe "#face_data" do
     it "should give similar results for small and large photos" do
-      big_obama = get_photo('big_obama.jpeg')
-      big_obama_data = nil
-      VCR.use_cassette('big_obama') do
-        big_obama_data = big_obama.face_data['tags'].first
-      end
+      big_obama_data = face_data('big_obama.jpeg')
+      small_obama_data = face_data('small_obama.jpeg')
       
-      small_obama = get_photo('small_obama.jpeg')
-      small_obama_data = nil
-      VCR.use_cassette('small_obama') do
-        small_obama_data = small_obama.face_data['tags'].first
-      end
+      big_eye_x = big_obama_data['tags'].first['eye_right']['x']
+      small_eye_x = small_obama_data['tags'].first['eye_right']['x']
       
-      big_obama_data['eye_right']['x'].should be_kind_of Float
-      small_obama_data['eye_right']['x'].should be_kind_of Float
-      big_obama_data['eye_right']['x'].should be_within(5.0).of(small_obama_data['eye_right']['x'])
+      big_eye_x.should be_kind_of Float
+      small_eye_x.should be_kind_of Float
+      
+      # compare positioning (percentages)
+      small_eye_x.should be_within(1.0).of(small_eye_x)
     end
   end
   
@@ -41,24 +57,19 @@ describe "retrieving face data" do
     end
     
     it "should scale the detection areas proportionally for large photos" do
-      big_obama = get_photo('big_obama.jpeg')
-      big_obama_data = nil
-      VCR.use_cassette('big_obama') do
-        big_obama_data = big_obama.face_data_as_px['tags'].first
-      end
+      big_obama_data = face_data_as_px('big_obama.jpeg')
+      small_obama_data = face_data_as_px('small_obama.jpeg')
       
-      small_obama = get_photo('small_obama.jpeg')
-      small_obama_data = nil
-      VCR.use_cassette('small_obama') do
-        small_obama_data = small_obama.face_data_as_px['tags'].first
-      end
+      big_eye_x = big_obama_data['tags'].first['eye_right']['x']
+      small_eye_x = small_obama_data['tags'].first['eye_right']['x']
       
-      big_obama_data['eye_right']['x'].should be_kind_of Float
-      small_obama_data['eye_right']['x'].should be_kind_of Float
-      big_obama_data['eye_right']['x'].should_not be_within(1.0).of(small_obama_data['eye_right']['x'])
+      big_eye_x.should be_kind_of Float
+      small_eye_x.should be_kind_of Float
+      big_eye_x.should_not be_within(1.0).of(small_eye_x)
       
-      scale = big_obama.width / small_obama.width.to_f
-      big_obama_data['eye_right']['x'].should be_within(5.0).of(small_obama_data['eye_right']['x'] * scale)
+      scale = big_obama_data['width'] / small_obama_data['width'].to_f
+      # compare positioning (px)
+      big_eye_x.should be_within(5.0).of(small_eye_x * scale)
     end
   end
 end
