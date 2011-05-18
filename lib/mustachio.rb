fb32f1d980f6461ced032e63a2dd597a87224fa3
Aidan Feldman <aidan.feldman@gmail.com>
2011-05-18 03:06:06 -0400

    moved stache listing to a config file; some refactoring around that

--- lib/mustachio.rb
+++ lib/mustachio.rb
@@ -4,16 +4,6 @@ require 'image_size'
 require 'face'
 
 # thanks to http://therantsandraves.com/?p=602 for the 'staches
-MUSTACHE = {
-  :filename => File.expand_path(File.join(File.join(File.dirname(__FILE__), '..', 'public', 'images', 'mustache_03.png'))),
-  :width => nil,
-  :height => nil,
-  :top_offset => -5.0,
-  :bottom_offset => -15.0
-}
-MUSTACHE[:width], MUSTACHE[:height] = ImageSize.new(File.new(MUSTACHE[:filename])).get_size
-
-FACE_POS_ATTRS = ['center', 'eye_left', 'eye_right', 'mouth_left', 'mouth_center', 'mouth_right', 'nose']
 
 Magickly.dragonfly.configure do |c|
   c.analyser.add :face_data do |temp_object|
@@ -24,7 +14,7 @@ Magickly.dragonfly.configure do |c|
     data = Mustachio.face_client.faces_detect(:file => temp_object.file, :attributes => 'none')['photos'].first # TODO use #face_data
     
     data['tags'].map! do |face|
-      FACE_POS_ATTRS.each do |pos_attr|
+      Mustachio::FACE_POS_ATTRS.each do |pos_attr|
         if face[pos_attr].nil?
           puts "WARN: missing position attribute '#{pos_attr}'"
         else
@@ -45,19 +35,22 @@ Magickly.dragonfly.configure do |c|
     
     commands = []
     photo_data['tags'].each do |face|
+      stache_num = 0 # TODO make this random
+      mustache = Mustachio.mustaches[stache_num]
+      
       # perform affine transform, such that the top-center
       # of the mustache is mapped to the nose, and the bottom-center
       # of the stache is mapped to the center of the mouth
       affine_params = [
-        [ MUSTACHE[:width]/2, MUSTACHE[:top_offset] ], # top-center of stache
+        [ mustache['width']/2, mustache['top_offset'] ], # top-center of stache
         [ face['nose']['x'], face['nose']['y'] ], # nose
         
-        [ MUSTACHE[:width]/2, MUSTACHE[:height] + MUSTACHE[:bottom_offset] ], # bottom-center of stache
+        [ mustache['width']/2, mustache['height'] + mustache['bottom_offset'] ], # bottom-center of stache
         [ face['mouth_center']['x'], face['mouth_center']['y'] ] # center of mouth
       ]
       affine_params_str = affine_params.map{|p| p.join(',') }.join(' ')
       
-      commands << "\\( #{MUSTACHE[:filename]} +distort Affine '#{affine_params_str}' \\)"
+      commands << "\\( #{mustache['file_path']} +distort Affine '#{affine_params_str}' \\)"
     end
     commands << "-flatten"
     
@@ -67,10 +60,7 @@ Magickly.dragonfly.configure do |c|
 end
 
 class Mustachio < Sinatra::Base
-  @@face_client = Face.get_client(
-    :api_key => (ENV['MUSTACHIO_FACE_API_KEY'] || raise("Please set MUSTACHIO_FACE_API_KEY.")),
-    :api_secret => (ENV['MUSTACHIO_FACE_API_SECRET'] || raise("Please set MUSTACHIO_FACE_API_SECRET."))
-  )
+  FACE_POS_ATTRS = ['center', 'eye_left', 'eye_right', 'mouth_left', 'mouth_center', 'mouth_right', 'nose']
   
   set :static, true
   set :public, 'public'
@@ -82,6 +72,25 @@ class Mustachio < Sinatra::Base
   class << self
     def face_client
       @@face_client
+    end
+    
+    def mustaches
+      @@mustaches
+    end
+    
+    def setup
+      @@face_client = Face.get_client(
+        :api_key => (ENV['MUSTACHIO_FACE_API_KEY'] || raise("Please set MUSTACHIO_FACE_API_KEY.")),
+        :api_secret => (ENV['MUSTACHIO_FACE_API_SECRET'] || raise("Please set MUSTACHIO_FACE_API_SECRET."))
+      )
+      
+      staches = YAML.load(File.read(File.join(File.dirname(__FILE__), '..', 'config', 'staches.yml')))
+      staches.map! do |stache|
+        stache['file_path'] = File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'staches', stache['filename']))
+        stache['width'], stache['height'] = ImageSize.new(File.new(stache['file_path'])).get_size
+        stache
+      end
+      @@mustaches = staches
     end
   end
   
@@ -99,4 +108,7 @@ class Mustachio < Sinatra::Base
   get '/face_api_dev_challenge' do
     haml :face_api_dev_challenge
   end
+  
+  
+  self.setup
 end
